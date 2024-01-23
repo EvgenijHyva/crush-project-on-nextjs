521b8e83fecd971c008bf41ad381e18dbe8fc22b
Evgeny <evgeny.hyvarinen@tietoevry.com>
2024-01-23 18:17:27 +0200

    Meals refactoring layouts

--- components/main-header.tsx
+++ components/main-header.tsx
@@ -1,23 +1,28 @@
 import Link from 'next/link';
 import logoImg from "@/assets/logo.png";
-import styles from "./main-heade.module.css";
+import styles from "./main-header.module.css";
 import Image from 'next/image';
+import MainHeaderBackround from '@/components/main-header-background'
+
 
 export default function MainHeader() {
-	return <header className={styles.header}>
-		<Link href="/" className={styles.logo}>
-			<Image src={logoImg} alt='logo: plate with food on it' priority />
-			Food
-		</Link>
-		<nav className={styles.nav}>
-			<ul>
-				<li>
-					<Link href="/meals">Browse Meals</Link>
-				</li>
-				<li>
-					<Link href="/community">Foodies Community</Link>
-				</li>
-			</ul>
-		</nav>
-	</header>
+	return <>
+		<MainHeaderBackround />
+		<header className={styles.header}>
+			<Link href="/" className={styles.logo}>
+				<Image src={logoImg} alt='logo: plate with food on it' priority />
+				Food
+			</Link>
+			<nav className={styles.nav}>
+				<ul>
+					<li>
+						<Link href="/meals">Browse Meals</Link>
+					</li>
+					<li>
+						<Link href="/community">Foodies Community</Link>
+					</li>
+				</ul>
+			</nav>
+		</header>
+	</>
 }
