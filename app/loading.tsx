308e5f04a256714a1189c265ddef79180b58f4b3
Evgeny <evgeny.hyvarinen@tietoevry.com>
2024-01-25 14:32:19 +0200

    Added loading component for testing

---
+++ app/loading.tsx
@@ -0,0 +1,5 @@
+import styles from "./loading.module.css";
+
+export default function LoadingPage() {
+	return (<h1 className={styles.loading}>Loading the contend, please wait...</h1>)
+}
--- lib/meals.ts
+++ lib/meals.ts
@@ -4,6 +4,7 @@ import { MealItem, MealItemProps } from './app-types';
 const db = sql("meals.db");
 
 export async function getMeals(): Promise<MealItemProps[]> {
+	await new Promise((resolve) => setTimeout(resolve, 5000));
 	return (db.prepare("SELECT * FROM meals").all() as MealItemProps[])
 		.map(item => new MealItem(item)); // fetch all rows
 };
