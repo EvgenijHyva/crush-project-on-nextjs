46294c46bc76a43923259036e9066bff20226d50
Evgeny <evgeny.hyvarinen@tietoevry.com>
2024-01-26 00:23:05 +0200

    template for share meal form

--- app/meals/[mealSlug]/not-found.tsx
+++ app/meals/[mealSlug]/not-found.tsx
@@ -6,10 +6,11 @@ import { useParams } from 'next/navigation';
 export default function NotFoundMeal() {
 	const { mealSlug } = useParams();
 	return <main className='not-found'>
-		<h1>Not found</h1>
-		<p> Unfortunately, but we couldn&apos;t find the requested meal</p>
+		<h1> Meal not found</h1>
+		<p>  Ooops we couldn&apos;t find the requested meal...</p>
 		<p> You tried to find:&nbsp;
 			<b className={styles["human-slug"]}>{humanizeString(mealSlug as string)}</b>
 		</p>
+		<p> Maybe you notice a typo?</p>
 	</main>
 }
